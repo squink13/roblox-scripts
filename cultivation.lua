ddaa5cb621499bc2836a5b81be0bf8441a8d23b5
squink13 <97664081+squink13@users.noreply.github.com>
2025-01-11 23:09:05 -0700

    Move anti afk to button

--- cultivation.lua
+++ cultivation.lua
@@ -200,25 +200,30 @@ local AutoProgressToggle = Tab:CreateToggle({
     end,
 })
 
--- anti afk
-local GC = getconnections or get_signal_cons
-if GC then
-    print("Player Idle Disabled")
-    for i, v in pairs(GC(player.Idled)) do
-        if v["Disable"] then
-            v["Disable"](v)
-        elseif v["Disconnect"] then
-            v["Disconnect"](v)
+local AntiAfkButton = Tab:CreateButton({
+    Name = "Anti-AFK",
+    Callback = function()
+        -- anti afk
+        local GC = getconnections or get_signal_cons
+        if GC then
+            print("Player Idle Disabled")
+            for i, v in pairs(GC(player.Idled)) do
+                if v["Disable"] then
+                    v["Disable"](v)
+                elseif v["Disconnect"] then
+                    v["Disconnect"](v)
+                end
+            end
+        else
+            print("Clicked button")
+            local VirtualUser = cloneref(game:GetService("VirtualUser"))
+            player.Idled:Connect(function()
+                VirtualUser:CaptureController()
+                VirtualUser:ClickButton2(Vector2.new())
+            end)
         end
-    end
-else
-    print("Clicked button")
-    local VirtualUser = cloneref(game:GetService("VirtualUser"))
-    player.Idled:Connect(function()
-        VirtualUser:CaptureController()
-        VirtualUser:ClickButton2(Vector2.new())
-    end)
-end
+    end,
+})
 
 storeForwardDirection()
 
